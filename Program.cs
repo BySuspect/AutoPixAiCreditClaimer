9d98995a809012fdc8dc32b1f158051fa9156359
Omer Y. Bayraktar <omerbayraktar43@gmail.com>
2024-08-22 22:54:23 +0300

    cleanup fresh dotnet base

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Windows.Forms;
-using AutoPixAiCreditClaimer.Pages;
-
 namespace AutoPixAiCreditClaimer
 {
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainPage());
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
--- README.md
+++ README.md
@@ -4,17 +4,4 @@
 
 ### [Download here](https://github.com/BySuspect/AutoPixAiCreditClaimer/releases/latest)
 
-## Preview
 
-![Preview](https://i.imgur.com/atrGohU.png)
-
-If you get an error upon launching, install [.NET Framework 4.7.2](https://dotnet.microsoft.com/en-us/download/dotnet-framework/net472).
-
-## Usage
-
--  Make sure your Google Chrome browser is installed
-- click the `Add New` button
-- Enter your account information
-- click the `Save` button
-- After your account information is added to the list, click the `Start Claim` button
-- And wait for progress end
